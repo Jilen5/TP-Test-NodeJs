1001da307519209876f6db9bcc887119e558cd0f
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2025-10-15 12:32:54 +0200

    sixieme test fini

--- tests/commerce.test.js
+++ tests/commerce.test.js
@@ -1,4 +1,5 @@
 const {Basket, addToBasket, removeFromBasket, transactionAllowed, payBasket} = require('../src/ecommerce');
+const assert = require('assert');
 
 test('Développer un test unitaire qui ajoute un produit au panier et vérifie que le montant total est correct. ', () => {
     const basket = new Basket();
@@ -48,3 +49,52 @@ test('Écrire un test fonctionnel complet pour vérifier le bon déroulement d u
     expect(userAccount.balance).toBe(20);
     }
 );
+
+function testAppEcommerce() {
+    try {
+        let basket = new Basket();
+        let item = {name: 'Produit A', price: 50};
+        addToBasket(basket, item);
+        assert.strictEqual(basket.totalPrice, 50);
+
+        basket = new Basket();
+        let item1 = {name: 'Produit A', price: 50};
+        let item2 = {name: 'Produit B', price: 30};
+        addToBasket(basket, item1);
+        addToBasket(basket, item2);
+        removeFromBasket(basket, item1);
+        assert.strictEqual(basket.totalPrice, 30);
+
+        basket = new Basket();
+        item = {name: 'Produit A', price: 50};
+        addToBasket(basket, item);
+        assert.strictEqual(basket.totalPrice, 50);
+        removeFromBasket(basket, item);
+        assert.strictEqual(basket.totalPrice, 0);
+
+        const userAccount1 = {balance: 100};
+        const userAccount2 = {balance: 30};
+        assert.strictEqual(transactionAllowed(userAccount1, 50), true);
+        assert.strictEqual(transactionAllowed(userAccount2, 50), false);
+
+        basket = new Basket();
+        item1 = {name: 'Produit A', price: 50};
+        item2 = {name: 'Produit B', price: 30};
+        addToBasket(basket, item1);
+        addToBasket(basket, item2);
+        const userAccount = {balance: 100};
+        payBasket(userAccount, basket);
+        assert.strictEqual(userAccount.balance, 20);
+
+        console.log('OK');
+        return true;
+    } catch (err) {
+        console.log('ERREUR');
+        return false;
+    }
+}
+
+module.exports = {testAppEcommerce};
+
+
+
