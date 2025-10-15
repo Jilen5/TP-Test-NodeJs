712f8dbdd91ed288b19ba7bcae2c0d9d88e2d688
Jilen Sebamalainathan <jilen.sebamalainathan@efrei.net>
2025-10-15 11:48:28 +0200

    deuxieme test fini

--- tests/commerce.test.js
+++ tests/commerce.test.js
@@ -7,3 +7,14 @@ test('Développer un test unitaire qui ajoute un produit au panier et vérifie q
     expect(basket.totalPrice).toBe(50);
     }
 );
+
+test('Développer un test unitaire qui supprime un produit du panier et vérifie que le montant total est correct', () => {
+    const basket = new Basket();
+    const item1 = {name: 'Produit A', price: 50};
+    const item2 = {name: 'Produit B', price: 30};
+    addToBasket(basket, item1);
+    addToBasket(basket, item2);
+    removeFromBasket(basket, item1);
+    expect(basket.totalPrice).toBe(30);
+    }
+);
